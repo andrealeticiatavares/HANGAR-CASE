8dfdbaa558dbbb628109f0db6c8bb55ddee6f29b
Andréa Letícia de Sousa Tavares <andrealeticiatavares@gmail.com>
2024-04-22 01:29:15 -0300

    feat: directs to pages

---
+++ app/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>hangar-case</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Akshar&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/Button-Ripple-Effect-Animation-Wave-Pulse.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="assets/css/Table-with-search-table.css">
+    <link rel="stylesheet" href="assets/css/Table-with-search.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md bg-body">
+        <div class="container-fluid"><a class="navbar-brand" href="index.php" style="font-family: Akshar, sans-serif;">Big <span style="color: rgb(137, 7, 7);">Bang</span> Theory Inc.</a></div>
+    </nav>
+    <div class="container">
+        <div>
+            <h1></h1>
+        </div>
+        <div class="m-5">
+            <div class="text-center m-3"><a class="btn btn-primary btn-lg border rounded-0 ripple-effect" href="resposta_1.php"  role="button" style="width: 300px;">Resposta 1</a></div>
+            <div class="text-center m-3"><a class="btn btn-success btn-lg border rounded-0 ripple-effect" href="resposta_2.php"  role="button" style="width: 300px;">Resposta 2</a></div>
+            <div class="text-center m-3"><a class="btn btn-warning btn-lg border rounded-0 ripple-effect" href="resposta_3.php"  role="button" style="width: 300px;">Resposta 3</a></div>
+            <div class="text-center m-3"><a class="btn btn-danger btn-lg border rounded-0 ripple-effect" href="resposta_4.php"  role="button" style="width: 300px;">Resposta 4</a></div>
+            <div class="text-center m-3"><a class="btn btn-primary btn-lg border rounded-0 ripple-effect" href="resposta_5.php"  role="button" style="width: 300px;background: var(--bs-indigo);">Resposta 5</a></div>
+            <div class="text-center m-3"><a class="btn btn-primary btn-lg border rounded-0 ripple-effect" href="resposta_6.php"  role="button" style="width: 300px;background: var(--bs-orange);">Resposta 6</a></div>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+    <script src="assets/js/DataTable---Fully-BSS-Editable-style.js"></script>
+    <script src="assets/js/Button-Ripple-Effect-Animation-Wave-Pulse-button-ripple-effect.js"></script>
+    <script src="assets/js/Table-with-search-table.js"></script>
+</body>
+
+</html>
